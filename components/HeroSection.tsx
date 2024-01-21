296fc03a42ea106247bb1af006bc48b33006d474
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-01-21 14:52:50 +0300

    update20

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -40,18 +40,16 @@ const HeroSection = () => {
         </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="about"
+        <Link to="about"
           activeClass="active"
           spy={true}
           smooth={true}
           offset={-100}
           duration={500}
+         
         >
-         
-         <HiArrowDown size={35} className="animate-bounce" />
-         
         </Link>
+        <HiArrowDown    size={35} className="animate-bounce" />
         
       </div>
     </section>
